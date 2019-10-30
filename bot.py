a41b2a949ca1e28e57bcd87607b935d8674e1c1d
TopicOffic <48392397+TopicOffic@users.noreply.github.com>
2019-10-30 16:53:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,48 +42,7 @@ async def clear(ctx, amount=2):
 
 @client.command()
 async def popo(ctx):
-   await ctx.send(random.choice(["https://cdn77.scoreuniverse.com/modeldir/data/posting/60/958/posting_60958_x_xl.jpg",
-               "https://cdn77.scoreuniverse.com/18eighteen/gallys/images_content/Adel_35036/13_tn.jpg",
-               "https://cdn77.scoreuniverse.com/18eighteen/gallys/images_content/Adel_35036/15_tn.jpg"
-               "http://cdn.hdpornpictures.com/2016-12-24/389207_14.jpg",
-               "http://cdn.hdpornpictures.com/2019-02-01/588632_02.jpg",
-               "http://cdn.hdpornpictures.com/2019-05-26/614361_13.jpg",
-               "http://cdn.hdpornpictures.com/2019-04-28/609832_15.jpg",
-               "http://cdn.hdpornpictures.com/2016-10-19/375505_09.jpg",
-               "https://images.nubiles.net/galleries2/kelsey_kage/3_new-comer/tn/9.jpg",
-               "https://images.nubiles.net/galleries2/kelsey_kage/3_new-comer/tn/16.jpg",
-               "http://cdn.hdpornpictures.com/2018-11-10/562308_04.jpg",
-               "http://cdn.hdpornpictures.com/2019-04-18/580378_02.jpg",
-               "http://cdn.hdpornpictures.com/2018-12-19/563356_09.jpg",
-               "http://cdn.hdpornpictures.com/2016-05-07/351751_06.jpg",
-               "http://cdn.hdpornpictures.com/2018-09-17/560261_14.jpg",
-               "http://cdn.hdpornpictures.com/2019-05-04/583615_12.jpg",
-               "http://galleries.momsteachingteens.com/p/blakeallie/images/31.jpg",
-               "http://galleries.momsteachingteens.com/p/blakeallie/images/35.jpg",
-               "http://galleries.momsteachingteens.com/p/blakeallie/images/23.jpg",
-               "http://galleries.sexymomma.com/042019/photo/audrey-aguilera-chasey-lain/images/11.jpg",
-               "http://galleries.sexymomma.com/042019/photo/audrey-aguilera-chasey-lain/images/10.jpg",
-               "http://galleries.sexymomma.com/042019/photo/audrey-aguilera-chasey-lain/images/14.jpg",
-               "http://galleries.sexymomma.com/042019/photo/audrey-aguilera-chasey-lain/images/3.jpg",
-               "http://galleries.sexymomma.com/042019/photo/audrey-aguilera-chasey-lain/images/5.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-sasha-heart/images/10.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-sasha-heart/images/12.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-sasha-heart/images/14.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-sasha-heart/images/9.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111176.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111174.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111179.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111180.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111182.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111185.jpg",
-               "http://cdn.affiliates.mature.nl/4/free/6952/pictures/111184.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/13.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/12.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/14.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/4.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/8.jpg",
-               "http://galleries.sexymomma.com/042019/photo/brenda-james-mandy-more/images/9.jpg",
-               "http://cdn.hdpornpictures.com/2017-08-15/455205_01.jpg"]))
+   await ctx.send(random.choice(["1,1"]))
 
    print("Qualcuno ha fatto il furbetto!!")
 
